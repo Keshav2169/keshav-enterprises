df8d37222b4b9f4bc7bf63bbf00bdf6734da58ba
Keshav2169 <keshav2169@gmail.com>
2026-04-20 23:05:56 +0530

    update: code

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+// vite.config.js
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: '/keshav-enterprises/',
+  base: "/", // or delete this line entirely
+  plugins: [react()],
 })
